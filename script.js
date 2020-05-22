ff238579b49abf97716434fdf416f77f4dc9757f
CGMcBride <connermcbride@ymail.com>
2020-05-22 10:36:13 -0500

    set API key

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// set variables for the one day Forecast to get city info on page
+$(document).ready(function){
 
+}
+$(".city").text();
+$(".wind").text();
+$(".humidity").text();
+
+// set a var for the API to get the weather information
+var apiKey = "c3be44ab7db984ed86cde2a02725a631";
