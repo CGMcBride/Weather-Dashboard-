ebd74d8e8a1169777e4c8eaa15114f03ef3e0ece
CGMcBride <connermcbride@ymail.com>
2020-05-23 11:02:12 -0500

    changed the structure and media query using boot

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // set variables for the one day Forecast to get city info on page
-$(document).ready(function){
+let dayOneForcast = "oneDayForcast";
+const oneDayCity = document.querySelector(".city");
+const oneDayWind = document.querySelector(".wind");
+const oneDayHumidity = document.querySelector(".humidity");
+const oneDayTemp = document.querySelector(".temp");
+const oneDayUvindex = document.querySelector(".UVindex");
 
-}
-$(".city").text();
-$(".wind").text();
-$(".humidity").text();
+// set an array for the cities to display a list of the cities
 
-// set a var for the API to get the weather information
-var apiKey = "c3be44ab7db984ed86cde2a02725a631";
+let city = [];
+
+// set a var for the API key to get the weather information
+let apiKey = "c3be44ab7db984ed86cde2a02725a631";
+let openWeather =
+	"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c3be44ab7db984ed86cde2a02725a631}";
